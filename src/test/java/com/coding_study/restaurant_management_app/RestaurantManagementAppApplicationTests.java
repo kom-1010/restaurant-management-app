d036791bf6ccfb999e598a233019d69a8e168df5
kom-1010 <kom-1010@naver.com>
2021-11-19 16:06:50 +0900

    init spring boot

---
+++ src/test/java/com/coding_study/restaurant_management_app/RestaurantManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding_study.restaurant_management_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
